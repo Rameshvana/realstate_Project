f4f2702356dd6b5ad780a8bfcc0d50b10b3180ca
Ramesh Vanaparthi <108477947+Rameshvana@users.noreply.github.com>
2024-02-17 17:49:59 +0530

    Update create.component.js

--- src/Components/Project/Create/create.component.js
+++ src/Components/Project/Create/create.component.js
@@ -42,15 +42,14 @@ import HomeComponent from '../../Home';
            <div className='col-10'>
               
               <form onSubmit={saveData}>
+
                 <div className='creat-top'>
                   <button type='button' className='btn btn-primary' onClick={()=> navigate('/dashbord')}>Back</button>
-                  <ul className='creat-links hn'>
+                  <ul className='creat-links'>
                     <li className='nav-item' ><a href='#'><FaHome className='creat-icon'/><b>Home</b></a></li>
                     <li className='nav-item' onClick={()=> navigate('/project-all')}><a href=''><FaProjectDiagram className='creat-icon'/><b>Project Manage</b></a></li>
                     <li className='nav-item'><MdOutlineUnarchive className='creat-icon'/><b>Create</b></li>
                   </ul>
-                  <div>
-                  </div>
                 </div>
                 
                 <div className='input-contaienr'>
